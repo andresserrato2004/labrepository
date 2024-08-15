9124fe239196cceb95d926a430d266d7b66bd29d
t-hash06 <tomasfelipe.p@gmail.com>
2024-08-15 15:39:59 -0500

    refactor: redirect / to /dashboard

---
+++ app/routes/_index/route.ts
@@ -0,0 +1,6 @@
+export { loader } from '@routes/index/loader';
+
+/**
+ * This module only re-exports the loader function from the loader module in the same directory.
+ * The only reason to do this is to redirect the user from / to /dashboard.
+ */
