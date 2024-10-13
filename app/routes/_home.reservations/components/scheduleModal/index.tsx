9d52b452fc4530ead0f67cc0d66bac70e7f8a533
t-hash06 <tomasfelipe.p@gmail.com>
2024-10-13 10:45:49 -0500

    feat: add create reservation modal layout

--- app/routes/_home.reservations/components/scheduleModal/index.tsx
+++ app/routes/_home.reservations/components/scheduleModal/index.tsx
@@ -1,16 +1,19 @@
 import type { ModalType } from '@components/modalForm/types';
 import type { ExtendedReservation } from '@database/types';
+import type { Key } from '@react-types/shared';
 import type { action } from '@routes/periods/action';
 
-import { ModalForm, toast } from '@components';
+import { HiddenInput, ModalForm, toast } from '@components';
 import { useModalForm } from '@components/modalForm/providers';
-import { useFetcherErrors } from '@hooks/fetcher';
+// import { useFetcherErrors } from '@hooks/fetcher';
+import { Autocomplete, AutocompleteItem } from '@nextui-org/autocomplete';
 import { Button } from '@nextui-org/button';
+import { TimeInput } from '@nextui-org/date-input';
 import { DatePicker } from '@nextui-org/date-picker';
-import { Input } from '@nextui-org/input';
+import { Textarea } from '@nextui-org/input';
 import { ModalBody, ModalFooter, ModalHeader } from '@nextui-org/modal';
 import { ResponseType } from '@services/shared/utility';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const getModalTitle = (modalType: ModalType) => {
 	switch (modalType) {
@@ -37,11 +40,14 @@ const getSuccessMessage = (modalType: ModalType) => {
 };
 
 export function ReservationsModal() {
+	const [userId, setUserId] = useState<Key>('');
+	const [classroomId, setClassroomId] = useState<Key>('');
+
 	const { modalType, fetcher, closeModal } = useModalForm<
 		ExtendedReservation,
 		typeof action
 	>();
-	const { clearErrors, createErrorProps } = useFetcherErrors(fetcher);
+	// const { clearErrors, createErrorProps } = useFetcherErrors(fetcher);
 
 	const isLoading = fetcher.state !== 'idle';
 	const isDetails = modalType === 'details';
@@ -60,45 +66,77 @@ export function ReservationsModal() {
 		}
 	});
 
+	const handleUserIdChange = (value: Key | null) => {
+		setUserId(value ?? '');
+	};
+
+	const handleClassroomIdChange = (value: Key | null) => {
+		setClassroomId(value ?? '');
+	};
+
 	return (
 		<ModalForm>
 			<ModalHeader className='text-2xl mt-4'>
 				<h2>{getModalTitle(modalType)}</h2>
 			</ModalHeader>
 			<ModalBody className='grid grid-cols-12 p-6 gap-6'>
-				<Input
-					label='Name'
-					name='name'
+				<Autocomplete
+					label='User to reserve'
 					variant='faded'
 					className='col-span-12'
+					isClearable={false}
+					selectedKey={userId}
+					onSelectionChange={handleUserIdChange}
 					isRequired={true}
-					onValueChange={clearErrors('name')}
-					isReadOnly={isDetails}
-					{...createErrorProps('name')}
-				/>
-				<DatePicker
-					label='Start date'
-					name='startDate'
+				>
+					<AutocompleteItem key='123' value='admin'>
+						Test User 1
+					</AutocompleteItem>
+					<AutocompleteItem key='456' value='user'>
+						Test user 2
+					</AutocompleteItem>
+				</Autocomplete>
+				<Autocomplete
+					label='Classroom'
 					variant='faded'
-					className='col-span-6'
-					granularity='day'
+					className='col-span-12'
+					isClearable={false}
+					selectedKey={classroomId}
+					onSelectionChange={handleClassroomIdChange}
 					isRequired={true}
-					showMonthAndYearPickers={true}
-					isReadOnly={isDetails}
-					{...createErrorProps('startDate')}
-				/>
+				>
+					<AutocompleteItem key='redes' value='admin'>
+						Test Classroom 1
+					</AutocompleteItem>
+					<AutocompleteItem key='software' value='user'>
+						Test Classroom 2
+					</AutocompleteItem>
+				</Autocomplete>
+				<HiddenInput name='role' value={userId} />
 				<DatePicker
 					label='End date'
-					name='endDate'
 					variant='faded'
 					className='col-span-6'
-					granularity='day'
 					isRequired={true}
-					showMonthAndYearPickers={true}
-					isReadOnly={isDetails}
-					{...createErrorProps('endDate')}
+					granularity='day'
 				/>
-				{/*TODO: Implement the rest of this form*/}
+				<TimeInput
+					label='Start hour'
+					variant='faded'
+					className='col-span-3'
+					isRequired={true}
+				/>
+				<TimeInput
+					label='End hour'
+					variant='faded'
+					className='col-span-3'
+					isRequired={true}
+				/>
+				<Textarea
+					label='Description'
+					variant='faded'
+					className='col-span-12'
+				/>
 			</ModalBody>
 			<ModalFooter>
 				<Button
